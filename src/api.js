656266a1bd904aaad3facbb0f327618a68fa1506
Taha <tahaviwtahtouhihihi@gmail.com>
2026-06-28 00:30:56 +0100

    Add try,catch to async Function

---
+++ src/api.js
@@ -0,0 +1,14 @@
+const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+
+async function getMovies(title) {
+    try {
+    const response = await fetch(`http://www.omdbapi.com/?apikey=[apiKey]&s=${title}`)
+    if (!response.ok) {
+        throw new Error(`HTTP error: Status ${response.status}`)
+    }
+    const data = await response.json()
+    return data
+    }catch (error) {
+        console.error(`Network Error: ${error}`)
+    }
+}
