e369676e576c8d05ddf6f44f150c9ad8880bbca5
Taha <tahaviwtahtouhihihi@gmail.com>
2026-07-02 21:42:55 +0100

    add loading test

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const searchBtn = document.querySelector("#searchBtn")
 const searchInput = document.querySelector("#searchInput")
 
 export async function searchHandler() {
+    const cardsContainer = document.querySelector("#results")
+    cardsContainer.innerHTML = 'Loading...'
     const result = await getMovies(searchInput.value);//input
     const movies = result.Search;
     return renderCards(movies);
