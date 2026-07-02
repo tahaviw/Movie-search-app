1f38b55efc117d7eb02b2f1d81601c8c448fe5ac
Taha <tahaviwtahtouhihihi@gmail.com>
2026-07-02 01:51:15 +0100

    fix http bug for Netlify

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ const apiKey = import.meta.env.VITE_OMDB_API_KEY;
 
 async function getMovies(title) {
     try {
-        const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${title}`)
+        const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${title}`)
         if (!response.ok) {
             throw new Error(`HTTP error: Status ${response.status}`)
         }
